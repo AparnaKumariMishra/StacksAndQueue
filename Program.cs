8326e8b5a891c9102614918dda2c8b1162452d90
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-18 15:16:51 +0530

    Add files via upload
    Added UC1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StackAndQueue
+{
+    internal class program
+    {
+        static void Main()
+        {
+            UC1Add UC1Add = new UC1Add();
+            UC1Add.Push(70);
+            UC1Add.Push(30);
+            UC1Add.Push(56);
+            UC1Add.Display();
+            Console.ReadKey();
+        }
+    }
+}
+

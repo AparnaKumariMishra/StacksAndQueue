c28b54bb018fbd1e99ea077a2b13e12f64a390b5
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-18 15:18:00 +0530

    Add files via upload
    Added UC2

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace UC2PeakAndPop
+{
+    internal class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            UC2 UC2 = new UC2();
+            UC2.Push(70);
+            UC2.Push(30);
+            UC2.Push(56);
+            UC2.Display();
+            UC2.Peek();// top.data
+            UC2.Pop();
+            UC2.IsEmpty();
+            UC2.Display();
+            Console.ReadKey();
+        }
+    }
+}

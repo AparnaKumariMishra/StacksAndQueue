0ec31986aa27f6068adf1e9a8039e6b7b97b9110
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-18 15:19:23 +0530

    Add files via upload
    Added UC3

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Queue
+{
+    public class program
+    {
+       public static void Main()
+        {
+            UC3Queue UC3Queue = new UC3Queue();
+            UC3Queue.UC3Queue(56);
+            UC3Queue.UC3Queue(56);
+            UC3Queue.UC3Queue(56);
+            UC3Queue.Display();
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+

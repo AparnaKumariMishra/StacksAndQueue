e0da458f5fd03bc9ca05866ca51a9481aee4b363
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-18 15:20:20 +0530

    Add files via upload
    Added UC4

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace UC4Dequeue
+{
+    internal class program
+
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            UC4Dequeue UC4Dequeue = new UC4Dequeue();
+            UC4Dequeue.Enqueue(56);
+            UC4Dequeue.Enqueue(30);
+            UC4Dequeue.Enqueue(70);
+           // UC4Dequeue.Dequeu(80);
+            UC4Dequeue.Display();// this.top=this.top.next
+            Console.ReadKey();
+            UC4Dequeue.Dequeu(80);
+            UC4Dequeue.Display();// this.top=this.top.next
+            Console.ReadKey();
+
+        }
+    }
+}
